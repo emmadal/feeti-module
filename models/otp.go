8e3c0fafdd6af3408ccef29b8d02833dd1ff94ee
emmadal <emmanueldalougou@gmail.com>
2024-12-28 12:01:15 +0000

    fix: apply suggestion from pr

--- models/otp.go
+++ models/otp.go
@@ -7,11 +7,11 @@ import (
 // Otp is the struct for OTP in the database
 type Otp struct {
 	ID          int64     `json:"id" gorm:"primaryKey;unique;not null;autoIncrement"`
-	Code        string    `json:"code" gorm:"type:varchar(6);not null;index" binding:"required,min=6,max=6,numeric"`
+	Code        string    `json:"code" gorm:"type:varchar(6);not null;uniqueIndex" binding:"required,min=6,max=6,numeric"`
 	IsUsed      bool      `json:"is_used" gorm:"type:boolean;not null;default:false"`
 	PhoneNumber string    `json:"phone_number" gorm:"type:varchar(15);not null;index" binding:"required,e164,min=11,max=14"`
 	KeyUID      string    `json:"key_uid" gorm:"type:varchar(100);not null;index" binding:"required,uuid"`
-	ExpiryAt    time.Time `json:"expiry_at" binding:"gtfield=CreatedAt"`
+	ExpiryAt    time.Time `json:"expiry_at" binding:"required,gtfield=CreatedAt,future"`
 	CreatedAt   time.Time `json:"created_at"`
 }
 
