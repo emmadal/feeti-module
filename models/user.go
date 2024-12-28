b481753f4f01d5b2ec5a36a5d41eb05106019bf6
emmadal <emmanueldalougou@gmail.com>
2024-12-28 10:54:20 +0000

    fix: apply patches from review

--- models/user.go
+++ models/user.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"time"
+
+	"google.golang.org/genproto/googleapis/type/decimal"
 )
 
 // User is the struct for a user
@@ -11,7 +13,7 @@ type User struct {
 	LastName    string    `json:"last_name" gorm:"type:varchar(100);not null" binding:"required,alpha"`
 	Email       string    `json:"email" gorm:"type:varchar(200)"`
 	PhoneNumber string    `json:"phone_number" gorm:"type:varchar(15);uniqueIndex;unique;not null" binding:"required,e164,min=11,max=14"`
-	DeviceToken string    `json:"device_token" gorm:"type:varchar(200);not null" binding:"required;min=10,max=200"`
+	DeviceToken string    `json:"device_token" gorm:"type:varchar(200);not null" binding:"required,min=10,max=200"`
 	Pin         string    `json:"pin" gorm:"type:varchar(100);not null" binding:"required"`
 	Quota       uint      `json:"quota" gorm:"type:bigint;default:0;not null"`
 	Locked      bool      `json:"locked" gorm:"type:boolean;default:false"`
@@ -23,14 +25,14 @@ type User struct {
 
 // Wallet is the struct for a wallet
 type Wallet struct {
-	ID        int64     `json:"id" gorm:"primaryKey;unique"`
-	UserID    int64     `json:"user_id" gorm:"type:bigint;not null" binding:"required,number,gt=0"`
-	User      User      `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Balance   uint      `json:"balance" gorm:"type:bigint;default:0;not null" binding:"required,number,min=0,max=50000000"`
-	Currency  string    `json:"currency" gorm:"type:varchar(3);default:XOF;not null" binding:"alpha,oneof=XOF GHS XAF GNH EUR USD"`
-	IsActive  bool      `json:"is_active" gorm:"type:boolean;default:true"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        int64           `json:"id" gorm:"primaryKey;unique"`
+	UserID    int64           `json:"user_id" gorm:"type:bigint;not null" binding:"required,number,gt=0"`
+	User      User            `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Balance   decimal.Decimal `json:"balance" gorm:"type:decimal(20,2);default:0;not null" binding:"required"`
+	Currency  string          `json:"currency" gorm:"type:varchar(3);default:XOF;not null" binding:"alpha,oneof=XOF GHS XAF GNH EUR USD"`
+	IsActive  bool            `json:"is_active" gorm:"type:boolean;default:true"`
+	CreatedAt time.Time       `json:"created_at"`
+	UpdatedAt time.Time       `json:"updated_at"`
 }
 
 // UserLogin is the struct for login
