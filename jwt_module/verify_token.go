3c39f7e09badc499247e05db085e96e91485a25a
emmadal <emmanueldalougou@gmail.com>
2024-12-28 04:43:09 +0000

    feat: create func to verify user token

---
+++ jwt_module/verify_token.go
@@ -0,0 +1,39 @@
+package jwt_modules
+
+import (
+	"errors"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+// VerifyToken verify the given token to get its payload.
+func VerifyToken(tokenString string, secretKey []byte) (int64, error) {
+
+	// verify the signature of the token
+	parsedToken, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("Invalid signature")
+		}
+		return secretKey, nil
+	})
+
+	// check if there is an error
+	if err != nil {
+		return 0, errors.New("Couldn't handle this token")
+	}
+
+	// check the validity of token
+	if tokenIsValid := parsedToken.Valid; !tokenIsValid {
+		return 0, errors.New("Invalid or Expired token")
+	}
+
+	// extract claims data in the token
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return 0, errors.New("Invalid claims")
+	}
+	// convert userID to uint
+	userID := claims["userID"].(float64)
+	return int64(userID), nil
+}
