0b5b920de3b408d2f1def2f22a866cb25aa99334
emmadal <emmanueldalougou@gmail.com>
2024-12-28 11:54:49 +0000

    refactor: refactor otp models

--- models/otp.go
+++ models/otp.go
@@ -4,20 +4,19 @@ import (
 	"time"
 )
 
-// OTP is the struct for OTP in the database
-type OTP struct {
+// Otp is the struct for OTP in the database
+type Otp struct {
 	ID          int64     `json:"id" gorm:"primaryKey;unique;not null;autoIncrement"`
-	Code        string    `json:"code" gorm:"type:varchar(6);not null" binding:"required,min=6,max=6,numeric"`
+	Code        string    `json:"code" gorm:"type:varchar(6);not null;index" binding:"required,min=6,max=6,numeric"`
 	IsUsed      bool      `json:"is_used" gorm:"type:boolean;not null;default:false"`
 	PhoneNumber string    `json:"phone_number" gorm:"type:varchar(15);not null;index" binding:"required,e164,min=11,max=14"`
 	KeyUID      string    `json:"key_uid" gorm:"type:varchar(100);not null;index" binding:"required,uuid"`
 	ExpiryAt    time.Time `json:"expiry_at" binding:"gtfield=CreatedAt"`
 	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
 }
 
-// CheckOTP is the struct for checking the OTP
-type CheckOTP struct {
+// CheckOtp is the struct for checking the OTP
+type CheckOtp struct {
 	Code        string    `json:"code" binding:"required,min=6,max=6,numeric"`
 	PhoneNumber string    `json:"phone_number" binding:"required,e164,min=11,max=14"`
 	KeyUID      string    `json:"key_uid" binding:"required,uuid"`
