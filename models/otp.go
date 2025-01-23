c780c1f50872cbd02bb6c2c8a368f707c11d541b
emmadal <emmanueldalougou@gmail.com>
2025-01-23 17:28:44 +0000

    revert: revert struct deleted

--- models/otp.go
+++ models/otp.go
@@ -9,7 +9,7 @@ type Otp struct {
 	ID          int64     `json:"id" gorm:"primaryKey;unique;not null;autoIncrement"`
 	Code        string    `json:"code" gorm:"type:varchar(10);not null;index" binding:"required,min=5,max=6,numeric"`
 	IsUsed      bool      `json:"is_used" gorm:"type:boolean;not null;default:false"`
-	PhoneNumber string    `json:"phone_number" gorm:"type:varchar(15);not null;index" binding:"required,e164,min=11,max=14"`
+	PhoneNumber string    `json:"phone_number" gorm:"type:varchar(15);not null;index" binding:"required,e164,max=14"`
 	KeyUID      string    `json:"key_uid" gorm:"type:varchar(100);not null;index" binding:"required,uuid"`
 	ExpiryAt    time.Time `json:"expiry_at" binding:"required"`
 	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
@@ -22,5 +22,9 @@ type CheckOtp struct {
 	PhoneNumber string    `json:"phone_number" binding:"required,e164,min=11,max=14"`
 	KeyUID      string    `json:"key_uid" binding:"required,uuid"`
 	ExpiryAt    time.Time `json:"expiry_at"`
-	CreatedAt   time.Time `json:"created_at"`
+}
+
+// NewOtp is the struct for creating a new OTP
+type NewOtp struct {
+	PhoneNumber string `json:"phone_number" binding:"required,e164,min=11,max=14"`
 }
