2ea4baabe0b8ca19957659650b062ad88e573120
emmadal <emmanueldalougou@gmail.com>
2025-02-27 15:11:33 +0000

    refactor: refactor module (#34)
    * refactor: refactor module
    
    * ci: fix ci/cd actions

--- jwt_module/create_token.go
+++ jwt_module/create_token.go
@@ -7,16 +7,16 @@ import (
 	jwt "github.com/golang-jwt/jwt/v5"
 )
 
-// GenerateToken generate a valid jwt token for 10 minutes
+// GenerateToken generate a valid jwt token for 30 minutes
 func GenerateToken(userID int64, secretKey []byte) (string, error) {
 	// check if the secret key and userID are valid
 	if userID <= 0 || secretKey == nil {
 		return "", errors.New("invalid secret key or userID")
 	}
 	// create a new token with the given userID
-	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"userID": userID,
-		"exp":    time.Now().Add(10 * time.Minute).Unix(), // expire in 10 minutes
+		"exp":    time.Now().Add(30 * time.Minute).Unix(), // expire in 30 minutes
 	})
 	// sign the token with the secret key
 	return token.SignedString(secretKey)
