98bb66e34e6eebe97a28b83537398f741c580957
emmadal <emmanueldalougou@gmail.com>
2025-05-17 16:26:26 +0000

    refactor: change cookie method

--- jwt_module/secure_cookie.go
+++ jwt_module/secure_cookie.go
@@ -1,35 +1,35 @@
 package jwt_module
 
 import (
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
 )
 
 // SetSecureCookie sets a JWT token in a cookie with secure settings
-func SetSecureCookie(w http.ResponseWriter, token string, domain string, isProduction bool) {
+func SetSecureCookie(c *gin.Context, token string, domain string, isProduction bool) {
 	// Create a new cookie with the token
 	cookie := &http.Cookie{
 		Name:     "ftk",
 		Value:    token,
 		Path:     "/",
 		Domain:   domain,
-		MaxAge:   int(30 * time.Minute.Seconds()), // Match token expiration time
-		HttpOnly: true,                            // Prevent JavaScript access
-		SameSite: http.SameSiteLaxMode,            // Protect against CSRF
+		MaxAge:   int(30 * time.Minute),   // Match token expiration time
+		HttpOnly: true,                    // Prevent JavaScript access
+		SameSite: http.SameSiteStrictMode, //
 	}
 
-	// Only set a Secure flag in production, or when HTTPS is used,
-	// This ensures cookies are only sent over HTTPS
-	if isProduction {
+	// Set Secure based on HTTPS usage
+	if c.Request.TLS != nil || c.GetHeader("X-Forwarded-Proto") == "https" {
 		cookie.Secure = true
 	}
 
 	// Set the cookie in the response
-	http.SetCookie(w, cookie)
+	http.SetCookie(c.Writer, cookie)
 }
 
 // ClearAuthCookie clears the authentication cookie
-func ClearAuthCookie(w http.ResponseWriter, domain string) {
+func ClearAuthCookie(c *gin.Context, domain string) {
 	cookie := &http.Cookie{
 		Name:     "ftk",
 		Value:    "",
@@ -37,7 +37,7 @@ func ClearAuthCookie(w http.ResponseWriter, domain string) {
 		Domain:   domain,
 		MaxAge:   -1,
 		HttpOnly: true,
-		SameSite: http.SameSiteLaxMode,
+		SameSite: http.SameSiteStrictMode,
 	}
-	http.SetCookie(w, cookie)
+	http.SetCookie(c.Writer, cookie)
 }
