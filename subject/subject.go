8f5832f642f9f87db74fa39320baa7d0a64181fb
emmadal <emmanueldalougou@gmail.com>
2025-06-03 22:12:16 +0000

    feat: add more NATS subjects

--- subject/subject.go
+++ subject/subject.go
@@ -1,14 +1,16 @@
 package subject
 
 const (
-	SubjectWalletCreate   = "wallet.create"
-	SubjectWalletGet      = "wallet.get"
-	SubjectWalletLock     = "wallet.lock"
-	SubjectWalletUnlock   = "wallet.unlock"
-	SubjectWalletBalance  = "wallet.balance"
-	SubjectWalletDeposit  = "wallet.deposit"
-	SubjectWalletWithdraw = "wallet.withdraw"
-	SubjectWalletDisable  = "wallet.disable"
+	SubjectWalletCreate       = "wallet.create"
+	SubjectWalletGet          = "wallet.get"
+	SubjectWalletLock         = "wallet.lock"
+	SubjectWalletUnlock       = "wallet.unlock"
+	SubjectWalletBalance      = "wallet.balance"
+	SubjectWalletDeposit      = "wallet.deposit"
+	SubjectWalletWithdraw     = "wallet.withdraw"
+	SubjectWalletDisable      = "wallet.disable"
+	SubjectWalletEnable       = "wallet.enable"
+	SubjectWalletCheckBalance = "wallet.check_balance"
 )
 
 const (
@@ -17,11 +19,16 @@ const (
 	SubjectUserLock    = "user.lock"
 	SubjectUserUnlock  = "user.unlock"
 	SubjectUserDisable = "user.disable"
+	SubjectUserEnable  = "user.enable"
 )
 
 const (
 	SubjectNotificationCreate = "notification.create"
 	SubjectNotificationGet    = "notification.get"
+	SubjectNotificationBulk   = "notification.bulk"
+	SubjectNotificationDelete = "notification.delete"
+	SubjectOTPCreate          = "otp.create"
+	SubjectOTPCheck           = "otp.check"
 )
 
 const (
